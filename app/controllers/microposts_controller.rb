d7f98cf5d6c1fd560e3e9e562db3e9d2ab21ec16
Lamine Kaba <Lkaba2003@yahoo.com>
2014-01-10 17:33:26 -0600

    Added user following

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,9 @@ class MicropostsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
   
+  def index
+  end
+  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,33 +3,39 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar",
-    password_confirmation: "foobar")                       
+	@user = User.new(name: "Example User", email: "user@example.com", password: "foobar",
+	password_confirmation: "foobar")                       
   end
 
   subject { @user }
+
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  
+
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
-   
+
   it { should be_valid }
   it { should_not be_admin }
-  
-  it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
-  it { should respond_to(:feed) }
 
   describe "with admin attribute set to 'true'" do
-    before do
-      @user.save!
-      @user.toggle!(:admin)
-    end
+	before do
+	  @user.save!
+	  @user.toggle!(:admin)
+	end
 
     it { should be_admin }
   end
@@ -147,21 +153,57 @@ describe User do
     end
     
     it "should destroy associated microposts" do
-      microposts = @user.microposts.to_a
-      @user.destroy
-      expect(microposts).not_to be_empty
-   
+	   microposts = @user.microposts.to_a
+	   @user.destroy 
+	   expect(microposts).not_to be_empty
+       microposts.each do |micropost|
+         expect(Micropost.where(id: micropost.id)).to be_empty
+       end
+
      
-     
-     describe "status" do
+    describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
      end
     end 
+  end
+  
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+    
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+    
+ 	describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
   end
 end
